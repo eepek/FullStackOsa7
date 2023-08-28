30a922435d2c82ddb570db44f5b355f77da86164
Eero-Pekka Karppinen <89537846+eepek@users.noreply.github.com>
2023-08-28 16:25:58 +0300

    Osa 7 tehtävät

---
+++ routed-anecdotes/src/App.js
@@ -0,0 +1,195 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import { useField } from './hooks/index'
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <Link to="/" style={padding}>anecdotes</Link>
+      <Link to="/createnew" style={padding}>create new</Link>
+      <Link to="/about" style={padding}>about</Link>
+    </div>
+  )
+}
+
+const Notification = ({ notification }) => {
+  const style = {
+    padding: 5,
+    borderStyle: "solid",
+    borderColor: "red",
+    borderWidth: 2
+  }
+
+  if (notification === '') {
+    return null
+  }
+
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
+    </ul>
+  </div>
+)
+
+const SingleAnecdote = ({ anecdote }) => {
+  return (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <span>has {anecdote.votes} votes</span><br /><br />
+    <span>for more info see <a href={anecdote.url}>{anecdote.url}</a></span><br/><br/>
+  </div>
+  )
+}
+
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+
+    <em>An anecdote is a brief, revealing account of an individual person or an incident.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      An anecdote is "a story with a point."</em>
+
+    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+  </div>
+)
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+
+    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+  </div>
+)
+
+const CreateNew = (props) => {
+
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+    props.setNotification(`A new anecdote ${content.value} created!`)
+    props.navigate('/')
+    setTimeout( () => {
+      props.setNotification('')
+    },5000)
+  }
+
+  //Oisin halunnut toteuttaa tän jotenkin spreadin avulla
+  //const { reset, ...content } = useFields('content'), mutta en saanut toimimaan nätisti, niin tässä nyt
+  //tällainen ratkaisu joka ainakin jättää errorit pois ja käyttää hookin functiota apunaan sekä ssäilyttää spreadin käytön
+  //input kentässä
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
+        <div>
+          content
+          <input { ...content} />
+        </div>
+        <div>
+          author
+          <input {...author}/>
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type='submit'>create</button><button type='reset'>reset</button>
+      </form>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id)) : null
+
+  const navigate = useNavigate()
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Notification notification={notification} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>}/>
+        <Route path="/anecdotes/:id" element={<SingleAnecdote anecdote={anecdote} />}/>
+        <Route path="/createnew" element={<CreateNew addNew={addNew} setNotification={setNotification} navigate={navigate}/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
